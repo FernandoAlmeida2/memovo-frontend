acc5657b7e4d6a759e7af97cecb02dca398af148
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-04-13 11:03:51 -0300

    chore: add dockerfile

--- src/components/Dashboard/Cards/Card.js
+++ src/components/Dashboard/Cards/Card.js
@@ -1,69 +1,36 @@
-import styled from "styled-components";
 import { FLASHCARD_COLORS } from "../../../constants/flashcardColors";
-import { BsFillPlayFill, BsFillCaretDownFill } from "react-icons/bs";
 import { useState } from "react";
+import FrontSide from "./Frontside";
+import Backside from "./BackSide";
 
-export default function Card({ card, currentCard, setCurrentCard, index }) {
+export default function Card({
+  card,
+  currentCard,
+  setCurrentCard,
+  index,
+  cardResult,
+}) {
   const color = FLASHCARD_COLORS.filter(
     (el) => el.categoryId === card.categoryId
   )[0].color;
-  const [showPinyin, setShowPinyin] = useState(false);
+  const [turnCard, setTurnCard] = useState(false);
 
-  return (
-    <>
-      {currentCard === index && (
-        <FrontSide color={color}>
-          <h1>{card.hanzi}</h1>
-          {showPinyin ? (
-            <IconDiv>
-              <BsFillCaretDownFill
-                size={"40px"}
-                onClick={() => setShowPinyin(!showPinyin)}
-              />
-              Hide pinyin
-            </IconDiv>
-          ) : (
-            <IconDiv>
-              <BsFillPlayFill
-                size={"40px"}
-                onClick={() => setShowPinyin(!showPinyin)}
-              />
-              Show pinyin
-            </IconDiv>
-          )}
-          <PinyinOption showPinyin={showPinyin}>{card.pinyin}</PinyinOption>
-        </FrontSide>
-      )}
-    </>
+  if (currentCard !== index) return;
+
+  return turnCard ? (
+    <Backside
+      color={color}
+      card={card}
+      currentCard={currentCard}
+      setCurrentCard={setCurrentCard}
+      cardResult={cardResult}
+    />
+  ) : (
+    <FrontSide
+      color={color}
+      setTurnCard={setTurnCard}
+      hanzi={card.hanzi}
+      pinyin={card.pinyin}
+    />
   );
 }
-
-const FrontSide = styled.div`
-  width: 500px;
-  height: 300px;
-  background-color: ${(props) => props.color};
-  font-size: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const IconDiv = styled.div`
-  cursor: pointer;
-  font-size: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const PinyinOption = styled.div`
-  display: ${(props) => (props.showPinyin ? "block" : "none")};
-  font-size: 40px;
-`;
-
-const BackSide = styled.div`
-  width: 400px;
-  height: 300px;
-  background-color: ${(props) => props.color};
-`;
--- src/pages/Dashboard/Learning/Learning.js
+++ src/pages/Dashboard/Learning/Learning.js
@@ -1,15 +1,21 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import Card from "../../../components/Dashboard/Cards/Card";
+import LearningResult from "../../../components/Dashboard/Cards/Result";
 import UserContext from "../../../contexts/UserContext";
 import { getLearningCards } from "../../../services/cardApi";
+import { LearningStyle } from "./LearningStyle";
 
 export default function LearningSession() {
   const { userData } = useContext(UserContext);
   const { numCards } = useParams();
-  const [cardsSession, setCardSession] = useState(null);
+  const [cardsSession, setCardSession] = useState([]);
   const [currentCard, setCurrentCard] = useState(0);
+  const [resultArray, setResultArray] = useState([]);
+
+  function cardResult(cardId, result) {
+    setResultArray([...resultArray, { cardId, result }]);
+  }
 
   useEffect(() => {
     getLearningCards(userData.token, numCards)
@@ -22,30 +28,26 @@ export default function LearningSession() {
       });
   }, [numCards, userData]);
 
-  if (!cardsSession) {
+  if (cardsSession.length === 0) {
     return <h1>Loading...</h1>;
   }
 
   return (
     <LearningStyle>
-      {cardsSession.map((card, index) => (
-        <Card
-          key={card.id}
-          index={index}
-          card={card}
-          currentCard={currentCard}
-          setCurrentCard={setCurrentCard}
-        />
-      ))}
+      {currentCard >= cardsSession.length ? (
+        <LearningResult resultArray={resultArray} />
+      ) : (
+        cardsSession.map((card, index) => (
+          <Card
+            key={card.id}
+            index={index}
+            card={card}
+            currentCard={currentCard}
+            setCurrentCard={setCurrentCard}
+            cardResult={cardResult}
+          />
+        ))
+      )}
     </LearningStyle>
   );
 }
-
-const LearningStyle = styled.div`
-  background-color: #efe4ce;
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
